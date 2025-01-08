40c5e2de8857c0719848f082754a33c75a7c7391
TH3-C0DE1 <marciopp1995@hotmail.com>
2025-01-08 15:05:18 +0000

    Minor Adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  
+const routes: Routes = 
+[
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
   {
@@ -19,10 +20,15 @@ const routes: Routes = [
   },
 
 ];
+
 @NgModule({
-  imports: [
+
+  imports: 
+  [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
+
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -31,4 +32,5 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
   bootstrap: [AppComponent],
   
 })
+
 export class AppModule {}
