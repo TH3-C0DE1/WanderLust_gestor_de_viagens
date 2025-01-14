0da93947038f1d385fa47cd3c4ca38eca44b7485
TH3-C0DE1 <marciopp1995@hotmail.com>
2025-01-14 17:12:57 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = 
 [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'trips', pathMatch: 'full' },
 
   {
     path: '',
