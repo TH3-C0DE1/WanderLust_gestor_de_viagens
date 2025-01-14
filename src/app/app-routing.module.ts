501b88c9d8b221811e446dcebac41bd7674ba117
TH3-C0DE1 <marciopp1995@hotmail.com>
2025-01-14 17:20:43 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,19 @@ const routes: Routes =
 [
   { path: '', redirectTo: 'hello-world', pathMatch: 'full' },
 
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
 
-  { path: 'trips', 
-    loadChildren: () => import('./trips/trips.module').then(m => m.TripsPageModule) 
-  },
+  // { path: 'trips', 
+  //   loadChildren: () => import('./trips/trips.module').then(m => m.TripsPageModule) 
+  // },
 
-  {
-    path: 'signup',
-    loadChildren: () => import('./profile/signup/signup.module').then( m => m.SignupPageModule)
-  },
+  // {
+  //   path: 'signup',
+  //   loadChildren: () => import('./profile/signup/signup.module').then( m => m.SignupPageModule)
+  // },
   {
     path: 'hello-world',
     loadChildren: () => import('./hello-world/hello-world.module').then( m => m.HelloWorldPageModule)
