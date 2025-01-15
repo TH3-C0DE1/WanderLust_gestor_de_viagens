6013a8dca77a774ede29256312851b89e71c1886
TH3-C0DE1 <marciopp1995@hotmail.com>
2025-01-15 18:00:41 +0000

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+console.log('Starting app...');
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.error('Error bootstrapping the app:', err);
+  });
